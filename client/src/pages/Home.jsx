8a960bf670efcbc35665f54755a4255472da814b
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-05 15:10:42 -0500

    Feat: tailwind test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,5 +5,9 @@ import useFetchedOrders from '../../hooks/useFetchedOrders';
 export default function Boilerplate() {
   const { orders, isLoading } = useFetchedOrders();
 
-  return isLoading ? <Loader /> : <div>Home</div>;
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div className="text-3xl font-bold underline text-center">Home</div>
+  );
 }
