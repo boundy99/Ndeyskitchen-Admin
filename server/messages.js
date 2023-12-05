8106684419cc9eeaf10c591739486bf66535029a
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-04 20:15:12 -0500

    Feat: messages added

---
+++ server/messages.js
@@ -0,0 +1,11 @@
+module.exports = {
+  USER_NOT_FOUND: 'User not found',
+  ORDERS_NOT_FOUND: 'Orders not found',
+  UNAUTHORIZED_REQUEST: 'Unauthorized request',
+  PASSWORD_INCORRECT: 'Password incorrect',
+  TOKEN_EXPIRED: 'Token expired',
+  ORDER_PLACED: 'Order placed',
+  NO_TOKEN_FOUND: 'No token found',
+  INVALID_TOKEN: 'Invalid token',
+  TOKEN_VALIDATED: 'Token validated',
+};
