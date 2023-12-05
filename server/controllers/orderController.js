79a1e771c40b8bcd4e8ffa808cd7cec4df868777
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-04 20:53:28 -0500

    Feat: orders fetching function from database

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,13 +1,17 @@
 const Order = require('../database/models/orderModel');
+const sendEmail = require('../scripts/sendEmail');
+
+const { ORDERS_NOT_FOUND } = require('../messages');
+
 async function getOrders(req, res) {
   try {
     const orders = await Order.find();
 
-    if (!orders) return res.status(404).json({ message: 'No orders found' });
+    if (!orders) return res.status(404).json({ message: ORDERS_NOT_FOUND });
 
-    return res.status(200).json({ orders });
+    return res.status(200).json(orders);
   } catch (err) {
-    console.log(err);
+    return res.status(500).json({ message: err });
   }
 }
 
