3606184ab8850d4a95ecb9f8a03c0dbb66ca4bd5
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-05 22:36:37 -0500

    Feat: Tailwind removed, vanilla css mode engaged

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,27 +2,21 @@ import { React, useEffect } from 'react';
 
 export default function Login() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <img className="w-32" src="../images/ndeys-kitchen.png" />
+    <div>
+      <img src="../images/ndeys-kitchen.png" />
 
-      <form className="flex flex-col items-center justify-center gap-3 border border-black px-12 py-10 rounded-md ">
+      <form>
         <header>
-          <h1 className="text-6xl mb-6 text-primary font-satisfy">Login</h1>
+          <h1>Login</h1>
         </header>
 
-        <label htmlFor="username" className="text-primary">
-          Username
-        </label>
-        <input id="username" className="input-box" type="text" required />
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" required />
 
-        <label htmlFor="password" className="text-primary">
-          Password
-        </label>
-        <input id="password" className="input-box" type="password" required />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" required />
 
-        <button className="border border-primary bg-primary text-white rounded-rd mt-6 p-1.5 text-xl px-4 hover:bg-white hover:text-primary transition ease-out duration-300">
-          Login
-        </button>
+        <button>Login</button>
       </form>
     </div>
   );
