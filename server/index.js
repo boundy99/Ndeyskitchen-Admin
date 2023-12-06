ba413b5bf2395f8c6683c9eef89717997505a305
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-06 18:09:00 -0500

    Feat: admin route added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const ordersRoutes = require("./routes/orders");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const ordersRoutes = require('./routes/orders');
+const adminsRoutes = require('./routes/admins');
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({message: "Welcome"});
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome' });
 });
 
 //routes
-app.use("/api/orders", ordersRoutes);
+app.use('/api/orders', ordersRoutes);
+app.use('/api/admins', adminsRoutes);
 
 module.exports = app;
