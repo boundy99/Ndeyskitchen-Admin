e30ccea10aeb2ce0e043807254d4728bafbed9d6
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-06 18:23:16 -0500

    Feat: model updated

--- server/database/models/adminModel.js
+++ server/database/models/adminModel.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const Adminchema = new Schema({
   username: { type: String, required: true, unique: true, maxlength: 20 },
-  password: { type: String, required: true, maxlength: 20 },
+  password: { type: String, required: true },
 });
 
-const Admin = mongoose.model('Order', Adminchema);
+const Admin = mongoose.model('Admin', Adminchema);
 
 module.exports = Admin;
