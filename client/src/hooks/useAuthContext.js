0ae3f5f39833bbb0d87fc8a5e01530fd8430c0ac
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-07 17:24:23 -0500

    Feat: import typo fixed

--- client/src/hooks/useAuthContext.js
+++ client/src/hooks/useAuthContext.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 export default function useAuthContext() {
   const context = useContext(AuthContext);
