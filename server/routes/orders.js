1613f22972e930b73a644cdbae6232f1126989bb
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-07 17:55:34 -0500

    Feat: admin validation middleware added to orders route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const middlewares = require('../middlewares');
 
-router.get('/', orderController.getOrders);
+router.get('/', middlewares.validateAdmin, orderController.getOrders);
 
 module.exports = router;
