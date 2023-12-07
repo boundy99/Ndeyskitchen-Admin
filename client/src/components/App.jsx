2824540a21957a49bff25009dd0f8c9292fb6b9a
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-07 17:35:06 -0500

    Feat: private route and public route

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import useAuthContext from '../hooks/useAuthContext';
 
 function App() {
+  const { admin } = useAuthContext();
+
+  const privateRoutes = (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+
+  const publicRoute = (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="pages">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
+        <div className="pages">{admin ? privateRoutes : publicRoute}</div>
       </BrowserRouter>
     </div>
   );
