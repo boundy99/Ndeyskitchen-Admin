af85bb628b7e7ed819124b6a2686fe6850375fdd
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-07 18:11:56 -0500

    Feat: checking when token expired or not

--- client/src/hooks/useFetchedOrders.js
+++ client/src/hooks/useFetchedOrders.js
@@ -1,13 +1,20 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import useAuthContext from '../hooks/useAuthContext';
 
 export default function useFetchedOrders() {
   const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { admin, dispatch } = useAuthContext();
+  const navigate = useNavigate();
+
   useEffect(() => {
     async function getOrders() {
       const response = await fetch('/api/orders', {
-        method: 'GET',
+        method: 'POST',
+        body: JSON.stringify({ admin }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -16,6 +23,15 @@ export default function useFetchedOrders() {
       const json = await response.json();
 
       if (!response.ok) console.log(json.message);
+
+      if (
+        json.Message === 'Token expired' ||
+        json.Message === 'User not found'
+      ) {
+        localStorage.removeItem('token');
+        dispatch({ type: 'LOGOUT' });
+        navigate('/login');
+      }
 
       setOrders(json);
       setIsLoading(false);
