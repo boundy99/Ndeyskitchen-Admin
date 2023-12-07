688a95d2d444d3966983cefb64d3d5752972c04c
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-07 17:24:37 -0500

    FeatL: Authprovider added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { AuthContextProvider } from './contexts/AuthContext';
 import '../dist/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
