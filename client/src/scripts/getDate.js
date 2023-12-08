e04387eef745465162d481bbab9bdab56857fbb1
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-08 17:21:56 -0500

    Feat: 1 removed

--- client/src/scripts/getDate.js
+++ client/src/scripts/getDate.js
@@ -26,16 +26,13 @@ const months = [
 const date = new Date();
 
 const currentDate = date.getDate();
-let currentMonth = date.getMonth() + 1;
+let currentMonth = date.getMonth();
 
 const dayOfWeek = date.getDay();
-
 const monthNumber = date.getMonth();
 
 const day = daysOfWeek[dayOfWeek];
-
 const month = months[monthNumber];
-
 const year = date.getFullYear();
 
 export default { currentDate, currentMonth, day, month, year };
