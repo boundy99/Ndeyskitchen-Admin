34111141f7f4c9b4f0cd8d2eb81e9372f8fb9dc2
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-07 19:09:06 -0500

    Feat: config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig, loadEnv, transformWithEsbuild } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [
-      react(),
-      {
-        name: 'treat-js-files-as-jsx',
-        async transform(code, id) {
-          if (!id.match(/src\/.*\.js$/)) return null;
-
-          return transformWithEsbuild(code, id, {
-            loader: 'jsx',
-            jsx: 'automatic',
-          });
-        },
-      },
-    ],
+    plugins: [react()],
     server: {
       port: env.VITE_PORT,
       proxy: {
