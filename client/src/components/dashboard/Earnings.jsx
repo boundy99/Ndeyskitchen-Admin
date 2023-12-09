022b73769bd7a12db159e23d4116d086a1a13607
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-08 19:02:45 -0500

    Feat: earnings components done

---
+++ client/src/components/dashboard/Earnings.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import getDate from '../../scripts/getDate';
+import getOrdersDetails from '../../scripts/getOrdersDetails';
+
+export default function Earnings() {
+  const { day, month, year } = getDate;
+
+  const { currentDayEarnings, currentMonthEarnings, currentYearEarnings } =
+    getOrdersDetails();
+
+  return (
+    <div className="earnings-container">
+      <h1>Earnings</h1>
+
+      <div className="earnings-content">
+        <table>
+          <thead>
+            <tr>
+              <th>{day}</th>
+              <th>{month}</th>
+              <th>{year}</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <td>D {currentDayEarnings}</td>
+            <td>D {currentMonthEarnings}</td>
+            <td>D {currentYearEarnings}</td>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
