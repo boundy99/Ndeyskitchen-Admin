bd31333b39092ee2bd825f54045f7c1891ef245d
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-10 17:31:45 -0500

    Feat: content added to the prder view

--- client/src/pages/OrderView.jsx
+++ client/src/pages/OrderView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import Loader from '../components/Loader';
 import OrderViewNabar from '../components/order-view/orderViewNavbar';
+import OrderViewContent from '../components/order-view/OrderViewContent';
 import useAuthContext from '../hooks/useAuthContext';
 import useFetchedOrders from '../hooks/useFetchedOrders';
 
@@ -10,11 +11,14 @@ export default function OrderView() {
   const { admin } = useAuthContext();
   const { orders, isLoading } = useFetchedOrders();
 
+  const filteredOrder = orders.filter(order => order._id === id);
+
   return isLoading ? (
     <Loader />
   ) : (
     <>
-      <OrderViewNabar orders={orders} admin={admin} id={id} />
+      <OrderViewNabar filteredOrder={filteredOrder} admin={admin} />
+      <OrderViewContent filteredOrder={filteredOrder} admin={admin} />
     </>
   );
 }
