f11bac350e1faa4ec9c6ac2855d7ebaa0de946ce
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-11 00:02:18 -0500

    Feat: style folder added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { AuthContextProvider } from './contexts/AuthContext';
-import '../dist/main.css';
+import '../style/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
