cfdef06b9fa6e26714d6a9e6b35999cfa7b9c2b9
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-10 22:26:40 -0500

    Feat: Messages updated

--- server/messages.js
+++ server/messages.js
@@ -2,6 +2,8 @@ module.exports = {
   ADMIN_NOT_FOUND: 'Admin not found',
   ORDERS_NOT_FOUND: 'Orders not found',
   ORDER_NOT_FOUND: 'Order not found',
+  ORDER_STATUS_UPDATED: 'Order status updated',
+  ORDER_STATUS_COULD_NOT_BE_UPDATED: 'Order status could not be updated',
   UNAUTHORIZED_REQUEST: 'Unauthorized request',
   PASSWORD_INCORRECT: 'Password incorrect',
   TOKEN_EXPIRED: 'Token expired',
